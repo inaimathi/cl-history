945207b57d0146ae27bb04de242d5e832926ef20
inaimathi <inaimathi@self>
2014-10-09 10:04:37 -0400

    -Tweaks to the toy election implementation (seems like full journaling doesn't get us VERY much here)

--- ToyElection.hs
+++ ToyElection.hs
@@ -2,29 +2,36 @@ module ToyElection where
 
 import History
 
+import Data.Function (on)
+import Data.List (sortBy)
 import Data.Map (Map, unionWith, fromList, toList)
 import qualified Data.Map as Map
 
 data Election = Election { voteCount :: Int
                          , candidateCount :: Int
                          , candidates :: [String]
-                         , score :: [(String, Tally, Counter)] }
+                         , ballots :: [Vote]
+                         , votingSystems :: [(String, Counter)] }
 instance Show Election where
-    show (Election vc _ cs s) = concat (unwords cs:" (":show vc:" votes)":results)
+    show (Election 0 _ cs _ _) = unwords cs ++ " (0 votes) []"
+    show (Election vc _ cs bs s) = concat (unwords cs:" (":show vc:" votes) ":results)
         where results = map singleRes s
-              singleRes (name, tally, _) = concat ["[ ", name, " => "
-                                                  , concatMap (\(k, v) -> k ++ ":" ++ show v ++ " ") $ toList tally 
-                                                  , "]"]
+              singleRes (name, fn) = concat ["[ ", name, " => "
+                                            , fst . head $ resultsBy bs fn
+                                            , " ]"]
 data Vote = Vote [String] deriving (Eq, Ord, Read, Show)
 
 type Tally = Map String Int
 type CandidateList = [String]
 type Counter = (Tally -> Vote -> Tally)
 
+resultsBy :: [Vote] -> Counter -> [(String, Int)]
+resultsBy vs fn = sorted . toList $ foldl fn Map.empty vs
+    where sorted = sortBy (flip compare `on` snd)
+
 applyVote :: Election -> Vote -> Election
 applyVote e v = e { voteCount = succ $ voteCount e
-                  , score = map tally $ score e }
-    where tally (name, t, fn) = (name, fn t v, fn)
+                  , ballots = v : (ballots e) }
 
 plurality :: Counter
 plurality tally (Vote ballot) = unionWith (+) tally . fromList $ zip (take 1 ballot) [1]
@@ -38,9 +45,9 @@ makeElection :: CandidateList -> Archive Election Vote
 makeElection cs = Archive
                   { into = applyVote
                   , write = fileWrite "election.arc"
-                  , state = Election 0 (length cs) cs 
-                            [ ("Borda Count", Map.empty, bordaCount)
-                            , ("Plurality", Map.empty, plurality) ]}
+                  , state = Election 0 (length cs) cs []
+                            [ ("Borda Count", bordaCount)
+                            , ("Plurality", plurality) ]}
 
 vote :: Archive Election Vote -> Vote -> IO (Archive Election Vote)
 vote = newEvent
